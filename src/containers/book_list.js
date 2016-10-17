c73b94a28ad80de46309b4a6947f00ddc302b7b5
Chinga Chaguluka <chinga.e.chaguluka@gmail.com>
2016-10-17 18:49:15 +0200

    Add return in the map function to return the list of books. App displaying book list.

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -4,19 +4,21 @@ import { connect } from 'react-redux';
 class BookList extends Component {
 
     renderList(){
-        
+
         return this.props.books.map((book) => {
-            <li className="list-group-item" key={book.title}>{book.tile}</li>
+            return (
+                <li className="list-group-item" key={book.title} >{book.title}</li>
+            );
         });
-
-
     }
 
     render(){
         return (
-            <ul className="list-group col-sm-4">
-                {this.renderList()}
-            </ul>
+            <div>
+                <ul className="list-group col-sm-4">
+                    {this.renderList()}
+                </ul>
+            </div>
         );
     }
 }
